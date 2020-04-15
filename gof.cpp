69629d867bf66efcc2ccde0f0382fe0048cdb9ce
eleonoradgr <eleonora.digregorio8@gmail.com>
2020-04-15 20:38:02 +0200

    new version of chunks

--- gof.cpp
+++ gof.cpp
@@ -2,23 +2,27 @@
 #include "GameOfLifeSeq.hpp"
 #include "GameOfLifePT.hpp"
 #include "GameOfLifeOMP.hpp"
+#include "GameOfLifePTRange2.hpp"
 
 
 int main(int argc, char *argv[] ){
-    GameOfLifeSeq  game(30000,600);
-    game.play(30, false);
+    GameOfLifeSeq  game(5,20000);
+    game.play(15, false);
 
     std::cout << "Parallel Version" << std::endl;
 
-    GameOfLifePT game2(30000, 600);
+    GameOfLifePT game2(5, 20000);
     //game2.printgrid() ;
-    game2.play(30, false, 20);
+    game2.play(15, false, 4);
     //game2.printgrid( std::string("output.txt"));
 
-    GameOfLifeOMP game3(30000, 600);
+    GameOfLifeOMP game3(5, 20000);
     //game2.printgrid() ;
-    game3.play(30, false,20);
+    //game3.play(15, false,4);
     //game2.printgrid( std::string("output.txt"));
+
+    GameOfLifePTRange2 game4 (5, 20000);
+    game4.play(15, false, 4);
 
 
 }
